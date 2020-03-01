1c69f765fc67cf2165cfeee7b5edd6b9c079981f
pdixit <p.dixit@student.tudelft.nl>
2020-03-01 04:52:25 +0100

    add code

--- Code/video_analysis.py
+++ Code/video_analysis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import Code.video_tools as video_tools
 import Code.feature_extraction as ft
 from Code.mfcc_talkbox import mfcc
+import scipy.io.wavfile as wav
 import math
 
 
@@ -75,6 +76,50 @@ def plot_difference_sum(ch):
     return sum
 
 
+def work_for_audio_per_second(path):
+    video_path = path + '.avi'
+    cap = cv2.VideoCapture(video_path)
+
+    audio_path = path + '.wav'
+    samplerate, samples = wav.read(audio_path)
+    # print(samplerate) 8000
+
+    plt.plot(samples)
+    plt.show()
+
+    # prev_frame = None
+    CH = []
+    # set video capture object to specific point in time
+
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    print("Frames per second {0}".format(fps))
+    count = 0
+
+    while cap.isOpened():
+        retVal, frame = cap.read()
+
+        if not retVal:
+            break
+
+        # == Do your processing here ==#
+        # if count % fps == 0:
+
+
+
+        # prev_frame = frame
+        count += 1
+
+        #
+        # cv2.imshow('Video', frame)
+        # cv2.waitKey()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 # Path to video file to analyse
 video1 = '/Users/pd/PycharmProjects/MA_Labs/mma-lab/Videos/video_07.mp4'
 video2 = '/Users/pd/PycharmProjects/MA_Labs/mma-lab/Videos/TUDelft_Ambulance_Drone.mp4'
@@ -89,6 +134,9 @@ E = 1  # seconds
 frame_count = video_tools.get_frame_count(video1) + 1
 frame_rate = video_tools.get_frame_rate(video1)
 
-do_work_per_second(video1)
-do_work_per_second(video2)
+# do_work_per_second(video1)
+# do_work_per_second(video2)
+
+work_for_audio_per_second('/Users/pd/PycharmProjects/MA_Labs/mma-lab/Videos/BlackKnight')
+
 
